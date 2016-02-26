a9005e0a56cd881c44b4486d1692947f0322d1e8
stoneand2 <stoneand2@users.noreply.github.com>
2016-02-26 13:00:04 -0600

    Third plot
    Adding a third plot to the .R file and updating the .pdf with plots in
    them.

--- Stone_PS4.R
+++ Stone_PS4.R
@@ -59,7 +59,16 @@ main.table[,"Winner Margin (%)"] <- as.numeric(gsub(pattern="%", replacement="",
 # Ordering by year
 main.table <- main.table[with(main.table, order(Year)),]
 
+
+
+
+
+##### THE PLOTS ######
+
 pdf(file="trends.pdf")
+
+par(mar=c(4.5, 5.7, 4.1, 2.1))
+layout(matrix(c(1,1,2,2), nrow=2, 2, byrow = TRUE))
 
 # Plot 1: Popular Vote Shares of Winning Democrats and Republicans Over Time
 
@@ -75,6 +84,7 @@ plot(main.table$Year[which(main.table$"Winner Party" == "Dem.")],
      main="Figure 1: Popular Vote Shares of Winning D/R Presidential Candidates Over Time", 
      cex.main=1)
 axis(side=1, labels=c(1824, 1860, 1900, 1940, 1980, 2016), at=c(1824, 1860, 1900, 1940, 1980, 2016))
+abline(.50, 0)
 
 lines(main.table$Year[which(main.table$"Winner Party" == "Dem.")], 
       y = main.table$"Winner popular vote (%)"[which(main.table$"Winner Party" == "Dem.")], 
@@ -106,7 +116,7 @@ plot(main.table$Year[which(main.table$"Winner Margin (%)" > .1)],
      col="steelblue4", 
      xlab="Year",
      ylab="Winner margin (millions of votes)", 
-     main="Figure 2: Winning Margin of Presidential Candidates Over Time", 
+     main="Figure 2: Popular Vote Margin of Presidential Candidates Over Time", 
      cex.main=1)
 abline(0,0)
 axis(side=1, 
@@ -127,10 +137,87 @@ legend("topleft",
        pch=c(17,16),
        col=c("steelblue4", "sienna2"), cex=0.8)
 
+
+layout(matrix(c(3,3,4,4), nrow=2, 2, byrow = TRUE))
+
+# Plot 3: 1904-2000 Election Winner Popular Vote Margins as Fraction of Entire U.S. 
+# Population at Time of Election
+
+# Loess smoothers fit for Democrats and Republicans
+
+# So, this plot lets us know what percentage of Americans would have needed to vote for the runner-up
+# to allow the runner-up to win the popular vote
+
+# Data on U.S. population, 1990-1999
+population <- read.csv("popdata1900_1999.csv", header=F, sep="")[,3:4]
+# Subtracting one from main.table$Year to match with year election was actually held in
+year.of.election <- main.table[21:44, "Year"] - 1
+
+# Function to match total U.S. population data to existing election data
+year.to.pop <- function(year){
+  population[,2][population[,1] == year]
+}
+# lapply() to apply function to each election year, unlist() to turn to vector, as.character() to make
+# non-factor, gsub() to remove commas, as.numeric() to make numeric
+total.pop.election.years <- as.numeric(gsub(",","",as.character(unlist(lapply(year.of.election, 
+                                                                              year.to.pop)))))
+# Making the election winners' popular vote margins numeric
+winners.margin <- as.numeric(main.table[21:44,"Winner Margin (total)"])
+
+# Finding winners' popular vote margin as function of total U.S. population, rounding to 3 decimals
+total.as.fraction.population <- round(winners.margin / total.pop.election.years, 3)
+
+# Table of candidates from this era
+results.1900s <- main.table[21:44,]
+  
+# The actual plot 
+plot(results.1900s[, "Year"][which(results.1900s$"Winner Party" == "Dem.")], 
+     total.as.fraction.population[which(results.1900s$"Winner Party" == "Dem.")],
+     xaxt="n", 
+     yaxt="n",
+     xlim=c(1900, 2000),
+     ylim=c(0,0.15),
+     pch=15, 
+     col="blue", 
+     xlab="Year",
+     ylab="Winning margin as percentage of 
+     total U.S. population", 
+     main="Figure 3: Popular Vote Margin of Presidential Candidates (1904-2000) 
+     As Percentage of Total U.S. Population", 
+     cex.main=1)
+points(results.1900s[, "Year"][which(results.1900s$"Winner Party" == "Rep.")], 
+       total.as.fraction.population[which(results.1900s$"Winner Party" == "Rep.")], col="red",
+       pch=16)
+abline(0,0, lty=2)
+axis(side=1, 
+     labels=c(1900, 1920, 1940, 1960, 1980, 2000), 
+     at=c(1900, 1920, 1940, 1960, 1980, 2000))
+axis(side=2, 
+     labels=seq(from=0, to=0.15, by=0.01), 
+     at=seq(from=0, to=0.15, by=0.01))
+text(main.table[21:44, "Year"], total.as.fraction.population, 
+     labels=main.table[21:44, "Winner"], cex= 0.5, pos=3)
+
+legend("topleft",
+       legend=c("Republican Winners", "Democrat Winners"), 
+       pch=c(16,17),
+       col=c("red", "blue"), cex=0.65, pt.cex = 1)
+
+loess.dems <- loess(total.as.fraction.population[which(results.1900s$"Winner Party" == "Dem.")] ~ 
+                      results.1900s[, "Year"][which(results.1900s$"Winner Party" == "Dem.")], 
+                    span=0.6)
+lines(y=predict(loess.dems), x=results.1900s[, "Year"][which(results.1900s$"Winner Party" == "Dem.")],
+      type="l", col="blue")
+
+loess.reps <- loess(total.as.fraction.population[which(results.1900s$"Winner Party" == "Rep.")] ~ 
+                      results.1900s[, "Year"][which(results.1900s$"Winner Party" == "Rep.")], 
+                    span=0.6)
+lines(y=predict(loess.reps), x=results.1900s[, "Year"][which(results.1900s$"Winner Party" == "Rep.")],
+      type="l", col="red")
+
+
 dev.off()
 
 
-
-
-
+# Scraping electoral college votes won by candidates
 
